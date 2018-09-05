8c4442914d779f14984bbe322763ed0e8d7c61f9
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-09-05 16:32:25 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -104,9 +104,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     #programmer_hash[:alan_kay][:known_for]["GUI"] = programmer_hash.delete("Object Orientation") 
+     #programmer_hash[:alan_kay][:known_for]["GUI"] = #programmer_hash.delete("Object Orientation") 
     # programmer_hash[:alan_kay][:known_for] = alans_new_info
-     programmer_hash.collect do |alan_kay|known_for|
+     ##programmer_hash.collect do |alan_kay|known_for|
        alans_new_info
      end
 end
