21315909af904c03f27891ae005e703965b0e9f6
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-09-05 16:44:48 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -128,5 +128,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-   programmer_hash[:dennis_ritchie]
+   programmer_hash[:dennis_ritchie] << "Assembly"
+   programmer_hash
 end
